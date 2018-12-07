c8126afa7dffe483c04c98d87bf32cff58caf576
Paolo Avesani <avesani@fbk.eu>
2018-12-07 12:07:24 -0500

    Added python code based on nibabel

---
+++ convert-tck-to-trk.py
@@ -0,0 +1,27 @@
+import nibabel as nib
+from nibabel.streamlines import Field
+from nibabel.orientations import aff2axcodes
+
+
+def main():
+
+ 	with open('config.json') as config_json:
+    	config = json.load(config_json)
+    
+	tck_file = config["tracks"]
+	anatomy_file = config["dwi"]
+	trk_file = './trk/track.trk'
+
+    nii = nib.load(anatomy_file)
+        
+    header = {}
+	header[Field.VOXEL_TO_RASMM] = nii.affine.copy()
+	header[Field.VOXEL_SIZES] = nii.header.get_zooms()[:3]
+	header[Field.DIMENSIONS] = nii.shape[:3]
+	header[Field.VOXEL_ORDER] = "".join(aff2axcodes(nii.affine))
+
+	tck = nib.streamlines.load(tck_file)
+	nib.streamlines.save(tck.tractogram, output_filename, header=header)
+
+if __name__ == '__main__':
+    main()
