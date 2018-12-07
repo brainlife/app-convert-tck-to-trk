32e132d9dc61d57655a6be0deb7bdbcebe3af061
Paolo Avesani <avesani@fbk.eu>
2018-12-07 12:59:16 -0500

    Fixed convert_tck_to_trk.py

--- convert_tck_to_trk.py
+++ convert_tck_to_trk.py
@@ -1,27 +1,31 @@
+#!/usr/bin/env python
+
+import json
 import nibabel as nib
 from nibabel.streamlines import Field
 from nibabel.orientations import aff2axcodes
 
 
 def main():
-	
-	with open('config.json') as config_json:
-		config = json.load(config_json)
+
+        with open('config.json') as config_json:
+                config = json.load(config_json)
     
-	tck_file = config["tracks"]
+	tck_file = config["tck"]
 	anatomy_file = config["dwi"]
 	trk_file = 'track.trk'
 
-    nii = nib.load(anatomy_file)
+        nii = nib.load(anatomy_file)
         
-    header = {}
+        header = {}
 	header[Field.VOXEL_TO_RASMM] = nii.affine.copy()
-	header[Field.VOXEL_SIZES] = nii.header.get_zooms()[:3]
+        header[Field.VOXEL_SIZES] = nii.header.get_zooms()[:3]
 	header[Field.DIMENSIONS] = nii.shape[:3]
 	header[Field.VOXEL_ORDER] = "".join(aff2axcodes(nii.affine))
 
 	tck = nib.streamlines.load(tck_file)
-	nib.streamlines.save(tck.tractogram, output_filename, header=header)
+        nib.streamlines.save(tck.tractogram, trk_file, header=header)
 
+        
 if __name__ == '__main__':
-    main()
+        main()
